699438d3269560bf7cfa21bd4b815ff9382c839a
Marek Noworolnik <marnow955@gmail.com>
2017-08-23 20:06:30 +0200

    Toolbar preview

--- src/main/java/com/github/marnow955/yourview/MainController.java
+++ src/main/java/com/github/marnow955/yourview/MainController.java
@@ -1,5 +1,10 @@
 package com.github.marnow955.yourview;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ToolBar;
+
 public class MainController {
 
+    @FXML
+    private ToolBar toolbar;
 }
