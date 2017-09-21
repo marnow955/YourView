2081db92646e72d08b8d26b9b234b08a115fa6b4
Marek Noworolnik <marnow955@gmail.com>
2017-09-21 18:42:48 +0200

    selection css

--- src/main/java/com/github/marnow955/yourview/controllers/ThumbViewController.java
+++ src/main/java/com/github/marnow955/yourview/controllers/ThumbViewController.java
@@ -2,12 +2,12 @@ package com.github.marnow955.yourview.controllers;
 
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.effect.DropShadow;
-import javafx.scene.effect.InnerShadow;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.TilePane;
-import javafx.scene.paint.Color;
 
 public class ThumbViewController {
 
@@ -18,6 +18,7 @@ public class ThumbViewController {
     private MainController mainController;
     private double cellWidth = 65.0;
     private double cellHeight = 65.0;
+    private double cellContainerPadding = 5.0;
 
     void injectMainController(MainController mainController) {
         this.mainController = mainController;
@@ -29,43 +30,23 @@ public class ThumbViewController {
             ImageView imageView = new ImageView(image);
             imageView.setFitWidth(cellWidth);
             imageView.setFitHeight(cellHeight);
+            StackPane cellContainer = new StackPane(imageView);
+            cellContainer.setPadding(new Insets(cellContainerPadding));
+            cellContainer.getStyleClass().add("thumbViewCell");
 
-            imageView.setOnMouseEntered(event -> {
-                DropShadow dropShadow = new DropShadow();
-                dropShadow.setOffsetX(6.0);
-                dropShadow.setOffsetY(4.0);
+            imageView.setOnMouseClicked(event ->
+                    mainController.selectImage(image)
+            );
 
-                imageView.setEffect(dropShadow);
-            });
-            imageView.setOnMouseExited(event -> {
-                if (thumbList.indexOf(image) == selectedIndex)
-                    setSelected(selectedIndex);
-                else
-                    imageView.setEffect(null);
-            });
-
-            imageView.setOnMousePressed(event -> {
-                InnerShadow innerShadow = new InnerShadow();
-                innerShadow.setOffsetX(4);
-                innerShadow.setOffsetY(4);
-                imageView.setEffect(innerShadow);
-            });
-
-            imageView.setOnMouseClicked(event -> mainController.selectImage(image));
-
-            thumbView.getChildren().add(imageView);
+            thumbView.getChildren().add(cellContainer);
         }
 
     }
 
     void setSelected(int index) {
         selectedIndex = index;
-        ImageView imageView = (ImageView) thumbView.getChildren().get(index);
-        InnerShadow innerShadow = new InnerShadow();
-        innerShadow.setOffsetX(4);
-        innerShadow.setOffsetY(4);
-        innerShadow.setColor(Color.web("0x3b596d"));
-        imageView.setEffect(innerShadow);
+        Node cell = thumbView.getChildren().get(index);
+        cell.getStyleClass().add("thumbViewCellSelected");
     }
 
     double getCellWidth() {
