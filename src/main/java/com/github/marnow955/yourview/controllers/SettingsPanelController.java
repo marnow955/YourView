186247c4c75852a6d9ea79182b675b7330fb6504
Marek Noworolnik <marnow955@gmail.com>
2017-10-14 15:32:48 +0200

    internatiolization of settings panel

--- src/main/java/com/github/marnow955/yourview/controllers/SettingsPanelController.java
+++ src/main/java/com/github/marnow955/yourview/controllers/SettingsPanelController.java
@@ -1,22 +1,39 @@
 package com.github.marnow955.yourview.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+
+import java.util.ResourceBundle;
 
 public class SettingsPanelController {
 
     @FXML
+    ResourceBundle resources;
+    private ObservableList<String> languageOptions;
+    private ObservableList<String> positionsOptions;
+    @FXML
     private ComboBox<String> langChoiceBox;
+    @FXML
+    private ComboBox<String> toolbarPosition;
+    @FXML
+    private ComboBox<String> thumbViewPosition;
 
     @FXML
     private void initialize() {
-        setLangList();
-    }
-
-    private void setLangList() {
-        langChoiceBox.getItems().addAll(
-                "English",
-                "Polish"
+        languageOptions = FXCollections.observableArrayList(
+                resources.getString("english"),
+                resources.getString("polish")
         );
+        positionsOptions = FXCollections.observableArrayList(
+                resources.getString("w_top"),
+                resources.getString("w_left"),
+                resources.getString("w_right"),
+                resources.getString("w_bottom")
+        );
+        langChoiceBox.setItems(languageOptions);
+        toolbarPosition.setItems(positionsOptions);
+        thumbViewPosition.setItems(positionsOptions);
     }
 }
